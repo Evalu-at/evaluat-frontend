39af3ba33540f957cd655ed69a9293339ac712b0
jvmls <jvmls@cesar.school>
2024-04-04 18:38:38 -0300

    feat: loginpage & grid added

---
+++ src/pages/loginPage.tsx
@@ -0,0 +1,16 @@
+import evaluAtIcon from '../assets/evaluAtIcon.svg';
+import { Input } from '../components/input';
+import { Button } from '../components/button';
+
+export function LoginPage() {
+  return (
+    <div className="gap-4 flex flex-col">
+      <img className="" src={evaluAtIcon} />
+      <Input className="rounded" placeholder="E-mail" />
+      <Input className="rounded" placeholder="Password" />
+      <Button className="hover:bg-slate-700 text-white bg-slate-950">
+        fon
+      </Button>
+    </div>
+  );
+}
