91f07c553e0504ef4605f0f28a3cabba6a1a1b68
Lucas Marques <lama@cesar.school>
2024-04-05 22:48:30 -0300

    Revert "feat: test new computer commit"

--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -4,7 +4,7 @@ import { Button } from '../components/button';
 
 export function LoginPage() {
   return (
-    <div className="gap-5 flex flex-col">
+    <div className="gap-4 flex flex-col">
       <img className="" src={evaluAtIcon} />
       <Input className="rounded" placeholder="E-mail" />
       <Input className="rounded" placeholder="Password" />
