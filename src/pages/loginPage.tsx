cc3559bfcccaeee7a503ac28c319235579a77efb
jvmls <jvmls@cesar.school>
2024-04-08 16:28:39 -0300

    feat: tailwind configure and login page

---
+++ src/pages/loginPage.tsx
@@ -0,0 +1,22 @@
+import evaluAtIcon from '../assets/evaluAtIcon.svg';
+import { Button } from '../components/ui/button';
+import { Input } from '../components/ui/input';
+
+export function LoginPage() {
+  return (
+    <div className="gap-5 flex flex-col">
+      <img className="" src={evaluAtIcon} />
+      <Input className="rounded text-slate-600/90" placeholder="E-mail" />
+      <Input className="rounded text-slate-600/90" placeholder="Password" />
+      <Button className="hover:bg-slate-700 text-white bg-slate-950">
+        Entrar
+      </Button>
+      <span className="text-slate-600">
+        Ainda não tem conta?{' '}
+        <a className=" underline" href="">
+          Clique Aqui.
+        </a>{' '}
+      </span>
+    </div>
+  );
+}
