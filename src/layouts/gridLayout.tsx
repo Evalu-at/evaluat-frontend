0c511e804318a6ac66ccec5bf5cc4c168a2f853f
jvmls <jvmls@cesar.school>
2024-04-09 16:20:59 -0300

    feat: frist implementation fo react hook form on login page

--- src/layouts/gridLayout.tsx
+++ src/layouts/gridLayout.tsx
@@ -2,6 +2,7 @@ export function GridLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="px-[50px] py-20  h-screen w-screen flex justify-center items-center">
       <div className="relative min-h-full min-w-full grid grid-cols-12 gap-8">
+        {/* <div className="bg-black/10"></div>
         <div className="bg-black/10"></div>
         <div className="bg-black/10"></div>
         <div className="bg-black/10"></div>
@@ -12,8 +13,7 @@ export function GridLayout({ children }: { children: React.ReactNode }) {
         <div className="bg-black/10"></div>
         <div className="bg-black/10"></div>
         <div className="bg-black/10"></div>
-        <div className="bg-black/10"></div>
-        <div className="bg-black/10"></div>
+        <div className="bg-black/10"></div> */}
         <div className="absolute flex flex-col place-self-center">
           {children}
         </div>
--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -2,20 +2,90 @@ import evaluAtIcon from '../assets/evaluAtIcon.svg';
 import { Button } from '../components/ui/button';
 import { Input } from '../components/ui/input';
 
+import { zodResolver } from '@hookform/resolvers/zod';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import z from 'zod';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+
 export function LoginPage() {
+  const formSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(2, { message: 'Senha muito curta' }),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    mode: 'onChange',
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   return (
     <div className="gap-5 flex flex-col">
       <img className="" src={evaluAtIcon} />
-      <Input className="rounded text-slate-600/90" placeholder="E-mail" />
-      <Input className="rounded text-slate-600/90" placeholder="Password" />
-      <Button className="hover:bg-slate-700 text-white bg-slate-950">
-        Entrar
-      </Button>
+      <Form {...form}>
+        <form className="flex flex-col gap-3">
+          <FormField
+            name="email"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    className="rounded text-slate-400"
+                    placeholder="Email"
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="password"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    className="rounded text-slate-400"
+                    placeholder="Senha"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            type="submit"
+            className="hover:bg-slate-700 text-white bg-slate-950 rounded"
+          >
+            Entrar
+          </Button>
+        </form>
+      </Form>
+
       <span className="text-slate-600">
         Ainda não tem conta?{' '}
         <a className=" underline" href="">
           Clique Aqui.
-        </a>{' '}
+        </a>
       </span>
     </div>
   );
